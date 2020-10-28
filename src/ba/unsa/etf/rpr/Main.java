4cf4343b27db81ea7ea2822de476ca4278d43160
dfermic2 <dfermic2@etf.unsa.ba>
2020-10-28 15:30:58 +0100

    Zadatak 2

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -1,8 +1,25 @@
 package ba.unsa.etf.rpr;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+    public static int sumaCifara(int br){
+        int sumaCifara = 0;
+        while (br != 0) {
+            int cifra = br % 10;
+            sumaCifara = sumaCifara + cifra;
+            br = br / 10;
+        }
+        return sumaCifara;
+    }
     public static void main(String[] args) {
-	// write your code here
+        Scanner in = new Scanner(System.in);
+        System.out.println("Unesite broj: ");
+        int n = in.nextInt();
+        System.out.println("Brojevi djeljivi sa sumom svojih cifara su: ");
+        for(int i = 1; i <= n; i++) {
+            if(i % sumaCifara(i) == 0)
+                System.out.println(i + " ");
+        }
     }
 }
